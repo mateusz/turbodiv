a7fb6067ea8727e54871059ed0f4ce31ce891f78
Mateusz Uzdowski <mateusz@silverstripe.com>
2015-04-05 14:56:57 +1200

    Try again - hijack the ResponseWriter. Probably wrong :-)

--- turbodiv.go
+++ turbodiv.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	//"golang.org/x/net/html"
+	"golang.org/x/net/html"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -79,25 +80,38 @@ func (t *Turbodiv) ServeHTTP(respWriter http.ResponseWriter, req *http.Request) 
 		backendUrlString = t.Config.BackendMappings["default"]
 	}
 	backendUrl, _ := url.Parse(backendUrlString)
+	// This is probably completely wrong - it feels like we should create only one reverse proxy per origin?
 	proxy := httputil.NewSingleHostReverseProxy(backendUrl)
 
 	// We cannot pass the response straight to the original client, because we want to do our substitutions.
-	amending := &amendingResponseWriter{
-		AmendFunc: func(buf []byte) {
-			fmt.Printf("Amending!\n")
-			// If response is HTML, parse it, looking for ESI markup, i.e. something like this:
-			//
-			//	<esi:include
-			//		src="//localhost:8002/ssorg/toolbar/profile"
-			//		processor="replace"
-			//		partitioner="Member" />
-			//html.ParseFragment
+	buffering := newBufferingResponseWriter(respWriter)
+	proxy.ServeHTTP(buffering, req)
 
-			// Invoke processors as needed depending on what is found in the markup.
-		},
-		OriginalWriter: respWriter,
+	var (
+		doc *html.Node
+		err error
+	)
+	if doc, err = html.Parse(&buffering.BodyBuffer); err == nil {
+		fmt.Printf("Parsing\n")
+		// Iteratively descend into the node tree to find esi tags.
+		var descend func(*html.Node)
+		descend = func(n *html.Node) {
+			if n.Type == html.ElementNode && n.Data == "esi:include" {
+				fmt.Printf("Found esi tag!\n")
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				descend(c)
+			}
+		}
+		descend(doc)
+
 	}
-	proxy.ServeHTTP(amending, req)
+
+	// Write parsed content.
+	buffering.BodyBuffer.Reset()
+	html.Render(&buffering.BodyBuffer, doc)
+
+	buffering.Flush()
 
 	// Check Cache-Control header to see if we need to do anything special in the future for this URL.
 	cacheControl := respWriter.Header()["Cache-Control"]
@@ -116,20 +130,46 @@ func (t *Turbodiv) ServeHTTP(respWriter http.ResponseWriter, req *http.Request) 
 
 }
 
-type amendingResponseWriter struct {
-	AmendFunc      func([]byte)
+// bufferingResponseWriter buffers the entire response so that it can be modified.
+// Once ready, the response can be flushed to the original destination.
+type bufferingResponseWriter struct {
+	BodyBuffer     bytes.Buffer
+	CodeBuffer     int
+	HeaderBuffer   http.Header
 	OriginalWriter http.ResponseWriter
 }
 
-func (rw *amendingResponseWriter) Header() http.Header {
-	return rw.OriginalWriter.Header()
+func newBufferingResponseWriter(respWriter http.ResponseWriter) *bufferingResponseWriter {
+	return &bufferingResponseWriter{
+		HeaderBuffer:   make(http.Header),
+		OriginalWriter: respWriter,
+	}
 }
 
-func (rw *amendingResponseWriter) Write(buf []byte) (int, error) {
-	rw.AmendFunc(buf)
-	return rw.OriginalWriter.Write(buf)
+// Meaningless before flush.
+func (rw *bufferingResponseWriter) Header() http.Header {
+	return rw.HeaderBuffer
 }
 
-func (rw *amendingResponseWriter) WriteHeader(code int) {
-	rw.OriginalWriter.WriteHeader(code)
+func (rw *bufferingResponseWriter) Write(buf []byte) (int, error) {
+	return rw.BodyBuffer.Write(buf)
+}
+
+func (rw *bufferingResponseWriter) WriteHeader(code int) {
+	rw.CodeBuffer = code
+}
+
+// Flush the content to the downstream writer.
+func (rw *bufferingResponseWriter) Flush() (int, error) {
+	copyHeader(rw.OriginalWriter.Header(), rw.HeaderBuffer)
+	rw.OriginalWriter.WriteHeader(rw.CodeBuffer)
+	return rw.OriginalWriter.Write(rw.BodyBuffer.Bytes())
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
 }
