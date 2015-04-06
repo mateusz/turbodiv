a326a89b3be2ae00dd752dc12d4d6d465bc8f651
Mateusz Uzdowski <mateusz@silverstripe.com>
2015-04-06 13:20:00 +1200

    After refactoring - works again.

--- processor.go
+++ processor.go
@@ -1,22 +1,172 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"golang.org/x/net/html"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"regexp"
 )
 
 type Processor interface {
-	Process(origReq *http.Request, src string, extraAttr map[string]string) ([]byte, error)
+	Process(origReq *http.Request, src string, extraAttr map[string]string) (*EsiResponse, error)
 }
 
-type ReplaceProcessor struct{}
+type IncludeProcessor struct {
+	Turbodiv *Turbodiv
+}
 
-func (p ReplaceProcessor) Process(origReq *http.Request, src string, extraAttr map[string]string) ([]byte, error) {
-	// Build the side-request.
-	//
-	// Check other Turbodiv attributes, invoke Partitioners as needed on the request (In).
-	//
-	// Send the request.
-	//
-	// Invoke Partitioners on the response (Out).
-	return nil, nil
+// Produce a new side request based on the original request.
+func (p IncludeProcessor) newSideReq(origReq *http.Request, urlStr string) *http.Request {
+	var url *url.URL
+	if urlStr == "" {
+		// Deep copy the URL. This is probably totally wrong :-)
+		newUrl := *origReq.URL
+		if origReq.URL.User != nil {
+			user := *(origReq.URL.User)
+			newUrl.User = &user
+		}
+		url = &newUrl
+	} else {
+		url, _ = url.Parse(urlStr)
+	}
+
+	// Find appropriate backend.
+	var backendUrlString string
+	var ok bool
+	if backendUrlString, ok = p.Turbodiv.Config.BackendMappings[url.Host]; !ok {
+		backendUrlString = p.Turbodiv.Config.BackendMappings["default"]
+	}
+	backendUrl, _ := url.Parse(backendUrlString)
+	// Overwrite the host & scheme by using the backend mapping.
+	url.Host = backendUrl.Host
+	url.Scheme = backendUrl.Scheme
+
+	// Bypass NewRequest for better control - e.g. we have parsed the url already.
+	req := &http.Request{
+		Method:     "GET",
+		URL:        url,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header:     make(http.Header),
+		Host:       backendUrl.Host,
+	}
+	copyHeader(req.Header, origReq.Header)
+	return req
+}
+
+func (p IncludeProcessor) esiReplacer(match []byte) []byte {
+	// The match string should contain only the esi tag as matched by regexp, might as well parse this properly.
+	var (
+		snippet *html.Node
+		err     error
+	)
+
+	if snippet, err = html.Parse(bytes.NewBuffer(match)); err != nil {
+		return []byte("<!-- invalid esi tag -->")
+	}
+
+	// Iteratively descend into the node tree to find esi tags.
+	var descend func(*html.Node) ([]byte, bool)
+	descend = func(node *html.Node) ([]byte, bool) {
+		if node.Type == html.ElementNode && node.Data == "esi:include" {
+
+			// Process the esi:include
+			extraAttr := make(map[string]string)
+			var src string
+			for _, attr := range node.Attr {
+				switch {
+				default:
+					extraAttr[attr.Key] = attr.Val
+				case attr.Key == "src":
+					src = attr.Val
+				}
+			}
+
+			// We have found a legit esi tag - no point in searching any further.
+			if src != "" {
+				return []byte("TESTING"), true
+			}
+
+		}
+
+		// Descend into sub-nodes, looking for first match. We are still within the string matched by regexp.
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			if replacement, found := descend(child); found {
+				return replacement, true
+			}
+		}
+
+		// Nothing of interest found. Could be no tag, or broken tag in the matched string.
+		return nil, false
+	}
+
+	var (
+		replacement []byte
+		found       bool
+	)
+
+	if replacement, found = descend(snippet); !found {
+		return []byte("<!-- invalid esi tag -->")
+	}
+
+	return replacement
+
+}
+
+func (p IncludeProcessor) Process(origReq *http.Request, src string, extraAttr map[string]string) (*EsiResponse, error) {
+	// Do the side-req.
+	sideReq := p.newSideReq(origReq, src)
+
+	client := &http.Client{}
+	resp, err := client.Do(sideReq)
+	if err != nil {
+		fmt.Printf("%#v\n", err)
+	}
+	defer resp.Body.Close()
+
+	// Drain the buffer so that we can perform the replacements.
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	// We cannot pass the response straight to the original client, because we want to do our substitutions.
+	// Use regexp, because we don't want to force normalisation of HTML.
+	// We are looking for esi tags, such as:
+	// <esi:include
+	//		src="http://some/url"
+	//		action="replace"
+	//		partitioner="Member" />
+	// Maybe use regexp templates instead? (the Expand fn)
+	esi := regexp.MustCompile("<esi:include[^>]*>")
+	newBytes := esi.ReplaceAllFunc(body, p.esiReplacer)
+
+	esiResp := &EsiResponse{
+		Code:   resp.StatusCode,
+		Header: resp.Header,
+		Body:   newBytes,
+	}
+
+	return esiResp, nil
+}
+
+type EsiResponse struct {
+	Code   int
+	Header http.Header
+	Body   []byte
+}
+
+func (e EsiResponse) WriteTo(to http.ResponseWriter) {
+	copyHeader(to.Header(), e.Header)
+	to.WriteHeader(e.Code)
+	to.Write(e.Body)
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
 }
--- turbodiv.go
+++ turbodiv.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"golang.org/x/net/html"
 	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
-	"net/http/httputil"
-	"net/url"
-	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -55,16 +49,8 @@ func NewTurbodiv(configPath string) (*Turbodiv, error) {
 	return turbodiv, nil
 }
 
-func bridge(from *httptest.ResponseRecorder, to http.ResponseWriter) {
-	to.WriteHeader(from.Code)
-	for name, value := range from.Header() {
-		to.Header()[name] = value
-	}
-	to.Write(from.Body.Bytes())
-}
-
+// We will hold up the main request, then perform side-requests and reassemble the main response.
 func (t *Turbodiv) ServeHTTP(respWriter http.ResponseWriter, req *http.Request) {
-	var backendUrlString string
 	var ok bool
 	originalUrl := req.URL.String()
 
@@ -76,98 +62,9 @@ func (t *Turbodiv) ServeHTTP(respWriter http.ResponseWriter, req *http.Request) 
 		delete(req.Header, "Cookie")
 	}
 
-	// Proxy the request by finding a relevant backend mapping.
-	if backendUrlString, ok = t.Config.BackendMappings[req.Host]; !ok {
-		backendUrlString = t.Config.BackendMappings["default"]
-	}
-	backendUrl, _ := url.Parse(backendUrlString)
-	// This is probably completely wrong - it feels like we should create only one reverse proxy per origin?
-	proxy := httputil.NewSingleHostReverseProxy(backendUrl)
-
-	// We cannot pass the response straight to the original client, because we want to do our substitutions.
-	buffering := newBufferingResponseWriter(respWriter)
-	proxy.ServeHTTP(buffering, req)
-
-	// Use regexp, because we don't want to force normalisation of HTML.
-	// We are looking for esi tags, such as:
-	// <esi:include
-	//		src="http://some/url"
-	//		processor="replace"
-	//		partitioner="Member" />
-	// Maybe use regexp templates instead? (the Expand fn)
-	esi := regexp.MustCompile("<esi:include[^>]*>")
-	newBytes := esi.ReplaceAllFunc(buffering.BodyBuffer.Bytes(), func(match []byte) []byte {
-		// We are going to replace the esi tag though, so we can parse this properly.
-		var (
-			snippet *html.Node
-			err     error
-		)
-
-		if snippet, err = html.Parse(bytes.NewBuffer(match)); err != nil {
-			return []byte("<!-- invalid esi tag -->")
-		}
-
-		// Iteratively descend into the node tree to find esi tags.
-		var descend func(*html.Node) ([]byte, bool)
-		descend = func(node *html.Node) ([]byte, bool) {
-			if node.Type == html.ElementNode && node.Data == "esi:include" {
-
-				// Process the esi:include
-				extraAttr := make(map[string]string)
-				var (
-					src       string
-					processor Processor
-				)
-
-				for _, attr := range node.Attr {
-					switch {
-					default:
-						extraAttr[attr.Key] = attr.Val
-					case attr.Key == "src":
-						src = attr.Val
-					case attr.Key == "processor":
-						switch attr.Val {
-						case "replace":
-							processor = ReplaceProcessor{}
-						}
-					}
-				}
-
-				// We have found a legit esi tag - no point in searching any further.
-				if src != "" && processor != nil {
-					if replacement, err := processor.Process(req, src, extraAttr); err == nil {
-						return replacement, true
-					}
-				}
-
-			}
-
-			// Descend into sub-nodes, looking for first match. We are still within the string matched by regexp.
-			for child := node.FirstChild; child != nil; child = child.NextSibling {
-				if replacement, found := descend(child); found {
-					return replacement, true
-				}
-			}
-
-			// Nothing of interest found. Could be no tag, or broken tag in the matched string.
-			return nil, false
-		}
-
-		var (
-			replacement []byte
-			found       bool
-		)
-
-		if replacement, found = descend(snippet); !found {
-			return []byte("<!-- invalid esi tag -->")
-		}
-
-		return replacement
-
-	})
-
-	buffering.ReplaceBody(newBytes)
-	buffering.Flush()
+	p := &IncludeProcessor{Turbodiv: t}
+	esiResponse, _ := p.Process(req, "", nil)
+	esiResponse.WriteTo(respWriter)
 
 	// Check Cache-Control header to see if we need to do anything special in the future for this URL.
 	cacheControl := respWriter.Header()["Cache-Control"]
@@ -184,54 +81,4 @@ func (t *Turbodiv) ServeHTTP(respWriter http.ResponseWriter, req *http.Request) 
 		}
 	}
 
-}
-
-// bufferingResponseWriter buffers the entire response so that it can be modified.
-// Once ready, the response can be flushed to the original destination.
-type bufferingResponseWriter struct {
-	BodyBuffer     bytes.Buffer
-	CodeBuffer     int
-	HeaderBuffer   http.Header
-	OriginalWriter http.ResponseWriter
-}
-
-func newBufferingResponseWriter(respWriter http.ResponseWriter) *bufferingResponseWriter {
-	return &bufferingResponseWriter{
-		HeaderBuffer:   make(http.Header),
-		OriginalWriter: respWriter,
-	}
-}
-
-// Meaningless before flush.
-func (rw *bufferingResponseWriter) Header() http.Header {
-	return rw.HeaderBuffer
-}
-
-func (rw *bufferingResponseWriter) Write(buf []byte) (int, error) {
-	return rw.BodyBuffer.Write(buf)
-}
-
-func (rw *bufferingResponseWriter) WriteHeader(code int) {
-	rw.CodeBuffer = code
-}
-
-// Flush the content to the downstream writer.
-func (rw *bufferingResponseWriter) Flush() (int, error) {
-	copyHeader(rw.OriginalWriter.Header(), rw.HeaderBuffer)
-	rw.OriginalWriter.WriteHeader(rw.CodeBuffer)
-	return rw.OriginalWriter.Write(rw.BodyBuffer.Bytes())
-}
-
-func (rw *bufferingResponseWriter) ReplaceBody(newBytes []byte) {
-	rw.BodyBuffer.Reset()
-	rw.BodyBuffer.Write(newBytes)
-	rw.Header().Set("Content-Length", fmt.Sprintf("%d", len(newBytes)))
-}
-
-func copyHeader(dst, src http.Header) {
-	for k, vv := range src {
-		for _, v := range vv {
-			dst.Add(k, v)
-		}
-	}
 }
